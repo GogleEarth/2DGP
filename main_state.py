7553fe74f73a175bbcb9298ac90b8b89d77288a2
GogleEarth <32813163+GogleEarth@users.noreply.github.com>
2017-12-26 14:27:04 +0900

    in main_state, remove 'esc' to end the game

--- main_state.py
+++ main_state.py
@@ -79,9 +79,7 @@ def handle_events(frame_time):
 
     events = get_events()
     for event in events:
-        if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            game_framework.quit()
-        elif event.type == SDL_QUIT:
+        if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
             game_framework.push_state(pause_state)
